5d7864704f352e13f5b4ce11ca423d331a38f6c9
Mertlim <dikushin.artm@bk.ru>
2024-07-27 20:47:28 +0300

    navbar and mobx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Burger from '../assets/Burger.svg'
+import Logo from '../assets/Logo.svg'
+import tg from '../assets/telegram.svg'
+
+export const Navbar = () => {
+	return (
+		<header className='w-full flex justify-center  '>
+			<div className='w-[1440px] flex justify-between items-center'>
+				<div className='flex items-center gap-6'>
+					<button>
+						<img src={Burger} alt="#" />
+					</button>
+					<div className='flex items-center'>
+						<img src={Logo} alt="#" />
+						<h1>Anime</h1>
+					</div>
+					<div className=''>
+						<input type="text" placeholder='Найти аниме...' />
+					</div>
+				</div>
+				<div>
+					<div className='flex gap-4'>
+						<img src={tg} alt="#" />
+						<button>Войти</button>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Movies } from '@/components/Movies'
+
+export const Home = () => {
+	return (
+		<div className='w-full flex justify-center'>
+			<div className='w-[1280px]'>
+				<Movies />
+			</div>
+		</div>
+	)
+}
