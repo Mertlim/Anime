7148298a0d3af1d5c71b60fe257f249e31e32e66
KiReally76L <141634933+KiReally76L@users.noreply.github.com>
2024-07-27 22:06:18 +0400

    ...

---
+++ src/store/AnimeApi.tsx
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { makeAutoObservable } from "mobx"
+
+
+export default new class anim{
+  animes = []
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+  // api
+  async fetchAnime() {
+    try {
+      const response = await axios.get('https://api.jikan.moe/v4/top/anime')
+      this.animes = response.data;
+    }
+    catch(e) {
+      console.error(e)
+    }
+  }
+}
+
